0dff4c105c8cdc3eadbca2b0ef549334088a39aa
Dani997 <36503822+Dani997@users.noreply.github.com>
2021-05-06 00:07:45 +0200

    Add files via upload

---
+++ programok/SQL_table_select/timer_class.hpp
@@ -0,0 +1,42 @@
+#include <chrono>
+
+using namespace std;
+
+using namespace std::chrono;
+
+class Timer
+{
+public:
+    void start()
+    {
+        StartTime = high_resolution_clock::now();
+        bRunning = true;
+    }
+    
+    void stop() 
+    {
+        EndTime = high_resolution_clock::now();
+        bRunning = false;
+    }
+    
+    double elapsedMicroseconds()
+    {
+        
+        if(bRunning)
+        {
+            stop();
+        }
+
+        return duration_cast<microseconds>(EndTime - StartTime).count();
+    }
+    
+    double elapsedSeconds()
+    {
+        return elapsedMicroseconds() / 1000000.0;
+    }
+
+private:
+    high_resolution_clock::time_point StartTime;
+    high_resolution_clock::time_point EndTime;
+    bool                              bRunning = false;
+};
