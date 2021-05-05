6437de66aa0628f31e2c8efc2bcdef24f95eb1a0
Dani997 <36503822+Dani997@users.noreply.github.com>
2021-05-06 00:16:42 +0200

    Add files via upload

---
+++ programok/Table_generator/readme.txt
@@ -0,0 +1,6 @@
+Ez a program generálja a JOIN -os kernelnél felhasznált adatbázis adatait.
+Ezeket importálni kell workbench alkalmazásban, illetve a kapcsolásokat létrehozni.
+
+Fordítás futtatás:
+c++ tablegen.cpp -o tablegen.out
+./tablegen.out
---
+++ programok/Table_generator/tablegen.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <time.h>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+
+    ofstream myfile;
+    int i;
+    srand(time(NULL));
+
+    myfile.open("speed_262144.csv");
+    myfile << "c1p1" << "," << "c2" << "," << "c3" << "," << "fk_s" << "," << "fk_xs" << "," << endl;
+    for(i=0; i<262144; i++)
+    myfile << i << "," << rand()%101<< "," << rand()%10001<< "," << rand()%131072<< "," << rand()%65536 << endl;
+
+    myfile.close();
+    myfile.open("speed_131072.csv");
+    myfile << "c1p1" << "," << "c2" << "," << "c3" << "," << "c4" << "," << "fk_xs" << endl;
+    for(i=0; i<65536; i++)
+    myfile << i << "," << rand()%101<< "," << rand()%10001<< "," << rand()%20001<< "," << i << endl;
+    for(i=65536; i<131072; i++)
+    myfile << i << "," << rand()%101<< "," << rand()%10001<< "," << rand()%20001<< "," <<  rand()%65536 << endl;
+
+
+    myfile.close();
+    myfile.open("speed_65536.csv");
+    myfile << "c1p1" << "," << "c2" << "," << "c3" << "," << "c4" <<  endl;
+    for(i=0; i<65536; i++)
+    myfile << i << "," << rand()%101<< "," << rand()%1001<< "," << rand()%10001 << endl;
+
+    myfile.close();
+
+    return 0;
+}
